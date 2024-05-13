f7400934e4dc4785a283424bbe92cc10fdd410d2
Arief Rachman Hakim <ariefrachmanhakim1001@gmail.com>
2024-05-13 23:28:24 +0700

    feat: add auth layout

--- src/route/index.jsx
+++ src/route/index.jsx
@@ -1,5 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "@/pages/login-page";
+import RegisterPage from "@/pages/register-page";
+import NotFoundPage from "@/pages/not-found-page";
+import HomePage from "@/pages/home-page";
+
 const AppRoute = () => {
-  return <div>AppRoute</div>;
+  return (
+    <Routes>
+      <Route path="*" element={<NotFoundPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/" element={<HomePage />} />
+      {/* <Route element={<AnonymousRoutes />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Route>
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/archives" element={<ArchivedNotes />} />
+        <Route path="/note/new" element={<CreateNotes />} />
+        <Route path="/notes/:id" element={<ShowNotes />} />
+      </Route> */}
+    </Routes>
+  );
 };
 
 export default AppRoute;
